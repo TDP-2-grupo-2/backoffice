6711a7975849c992250eaaa585a53174dc6abe7c
agustina235 <asegura@fi.uba.ar>
2023-05-12 22:52:57 -0300

    Feat: users Table

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,9 +1,81 @@
 import React from "react"
+import { Typography } from "@mui/material"
+import {TableView} from "../components/TableView"
 
 
 
-export const Users = () =>{
+const tableData = [
+    {
+      id: 1,
+      userName: 'pepe',
+      userEmail: 'una descripcion',
+      amount_report: '5',
+      motive: 'spam'
+    },
+    {
+      id: 2,
+      userName: 'raul',
+      userEmail: 'una descripcion',
+      amount_report: '3',
+      motive: 'spam'
+    },
+    {
+      id: 3,
+      userName: 'julia',
+      userEmail: 'una descripcion',
+      amount_report: '10',
+      motive: 'spam'
+    },
+    {
+      id: 4,
+      userName: 'agustina',
+      userEmail: 'una descripcion',
+      amount_report: '2',
+      motive: 'spam'
+    },
+    {
+      id: 5,
+      userName: 'sol',
+      userEmail: 'una descripcion',
+      amount_report: '1',
+      motive: 'spam'
+    },
+    {
+      id: 6,
+      userName: 'rama',
+      userEmail: 'una descripcion',
+      amount_report: '2',
+      motive: 'spam'
+    },
+    {
+      id: 7,
+      userName: 'pepe2',
+      userEmail: 'una descripcion',
+      amount_report: '5',
+      motive: 'spam'
+    },
+    
+  
+  ];
+
+
+export const Users= () =>{
+    let columns = ["Usuario", "Email", "Cantidad de Denuncias", "Motivo Principal"]
+    let information = ["userName", "userEmail", "amount_report", "motive"]
     return (
-        <div>Users</div>
-    );
+        <>
+            <Typography  variant="h3" align="top">
+                Usuarios Denunciantes
+            </Typography>
+            <br></br>
+            <br></br>
+            <TableView 
+                columns={columns}
+                infoToShow={information}
+                tableInfo={tableData}
+                isEvent={false}
+            />    
+           
+        </>
+    )
 }
