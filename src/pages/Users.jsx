f6067e438ce8c8fdbe8471772e4596082e1cfa51
agustina235 <asegura@fi.uba.ar>
2023-05-13 18:47:56 -0300

    Feat: conexion to back get attendes that reported

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,91 +1,103 @@
-import React, { useState } from "react"
-import { Typography } from "@mui/material"
+import React, { useState, useEffect } from "react"
+import { Typography, CircularProgress } from "@mui/material"
 import {TableView} from "../components/TableView"
 import { DateFilter } from "../components/DateFilter";
 
 
-const tableData = [
-    {
-      id: 1,
-      userName: 'pepe',
-      userEmail: 'una descripcion',
-      amount_report: '5',
-      motive: 'spam'
-    },
-    {
-      id: 2,
-      userName: 'raul',
-      userEmail: 'una descripcion',
-      amount_report: '3',
-      motive: 'spam'
-    },
-    {
-      id: 3,
-      userName: 'julia',
-      userEmail: 'una descripcion',
-      amount_report: '10',
-      motive: 'spam'
-    },
-    {
-      id: 4,
-      userName: 'agustina',
-      userEmail: 'una descripcion',
-      amount_report: '2',
-      motive: 'spam'
-    },
-    {
-      id: 5,
-      userName: 'sol',
-      userEmail: 'una descripcion',
-      amount_report: '1',
-      motive: 'spam'
-    },
-    {
-      id: 6,
-      userName: 'rama',
-      userEmail: 'una descripcion',
-      amount_report: '2',
-      motive: 'spam'
-    },
-    {
-      id: 7,
-      userName: 'pepe2',
-      userEmail: 'una descripcion',
-      amount_report: '5',
-      motive: 'spam'
-    },
-    
+
   
-  ];
 
 
 export const Users= () =>{
     let columns = ["Usuario", "Email", "Cantidad de Denuncias", "Motivo Principal"]
     let information = ["userName", "userEmail", "amount_report", "motive"]
+    const [ loading, setLoading ] = useState( true );
+    const APIURL = 'https://event-service-solfonte.cloud.okteto.net'
+
+
+    const [attendes, setAttendes] = useState([])
     const [from, setFrom] = useState(null)
     const [until, setUntil] = useState(null)
+
+    const handleRequest = () => {
+
+    }
+
+    async function getReportAttendes(token, from, until){
+      const paramsUpload = {
+          method: "GET",
+          headers: {
+              'Authorization': `Bearer ${token}`,
+              'Content-Type': 'application/json',
+          }
+      };
+      let url = ""
+      console.log(token)
+      if (from !== null && until !== null){
+           url = `${APIURL}/admins/reports/attendees?from_date=${from}&to_date=${until}`;
+      } else if ( from !== null){
+        url = `${APIURL}/admins/reports/attendees?from_date=${from}`;
+      } else if (until !== null){
+        url = `${APIURL}/admins/reports/attendees?to_date=${until}`;
+      } else {
+        url = `${APIURL}/admins/reports/attendees`;
+      }
+      const response = await fetch(
+          url,
+          paramsUpload
+      );
+      const jsonResponse = await response.json();
+  
+      if (response.status === 200){
+          if(!jsonResponse.status_code){
+              console.log(jsonResponse)
+              setAttendes(jsonResponse['message'])
+              setLoading(false)
+          }
+      }     
+  }
+  
+  useEffect( () => {
+    getReportAttendes(localStorage.getItem('token'), null, null);
+  }, []);
+  
+
     return (
-        <>
-            <Typography  variant="h3" align="top">
-                Usuarios Denunciantes
-            </Typography>
-            <br></br>
-            <br></br>
-            <DateFilter
-                from = {from}
-                until = {until}
-                setFrom = {setFrom}
-                setUntil = {setUntil}
-            />
-            <br></br>
-            <br></br>
-            <TableView 
-                columns={columns}
-                infoToShow={information}
-                tableInfo={tableData}
-                isEvent={false}
-            />    
-           
-        </>
+            !loading ?
+            <div className="reportedAttendes">
+                <Typography  variant="h3" align="top">
+                    Usuarios Denunciantes
+                </Typography>
+                <br></br>
+                <br></br>
+                <DateFilter
+                    from = {from}
+                    until = {until}
+                    setFrom = {setFrom}
+                    setUntil = {setUntil}
+                    handleRequest = {handleRequest}
+                />
+                <br></br>
+                <br></br>
+                {attendes.length > 0 ?
+                    <TableView 
+                        columns={columns}
+                        infoToShow={information}
+                        tableInfo={attendes}
+                        isEvent={false}
+                    />
+                  :  <h2 style={{marginTop:"3rem", textAlign:"center"}}>No hay usuarios denunciantes.</h2>    
+                }
+            </div>
+            :<CircularProgress 
+            style={{
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              height: '100vh',
+              margin: 'auto',
+              width: '10vw'
+            }}
+          />    
     )
 }
