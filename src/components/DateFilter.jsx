55dc87cbf0782592a5a1fbf7d41e75ec322b7cb5
agustina235 <asegura@fi.uba.ar>
2023-05-13 18:53:12 -0300

    Handle date filter

--- src/components/DateFilter.jsx
+++ src/components/DateFilter.jsx
@@ -12,23 +12,37 @@ import dayjs, { Dayjs } from 'dayjs';
 
 export const DateFilter = (props) => {
 
+
+    const handleFrom = (event) => {
+        props.setFrom(event); 
+        props.handleRequest()
+    }
+    
+    const handleUntil = (event) => {
+        props.setUntil(event); 
+        props.handleRequest()
+    }
+
     return (
+        <>
         <LocalizationProvider dateAdapter={AdapterDayjs}>
             <DemoContainer components={['DatePicker', 'DatePicker']}>
                 <DatePicker
                         label="Desde"
                         value={props.from}
                         maxDate={props.until || dayjs(Date.now())}
-                        onChange={(event) => props.setFrom(event)}
+                        onChange={(event) => handleFrom(event)}
                     />
                 <DatePicker
                     label="Hasta"
                     value={props.until}
                     minDate={props.from}
                     maxDate={dayjs(Date.now())}
-                    onChange={(event) => props.setUntil(event)}
+                    onChange={(event) => handleUntil(event)}
                 />
             </DemoContainer>
             </LocalizationProvider>
+
+        </>   
     )
 }
